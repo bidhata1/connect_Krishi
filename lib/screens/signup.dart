9688cb0e7f68ea09ecc426e78c87d4a422d73eb6
bidhata1 <pandey.bidhata58@gmail.com>
2023-05-27 19:31:43 +0545

    signup screen

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:connect_krishi/screens/login.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -11,6 +11,99 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('assets/img/logo.png')),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 25.0, right: 25.0, top: 50, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter your name'),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 25.0, right: 25.0, top: 20, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 25.0, right: 25.0, top: 20, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 25.0, right: 25.0, top: 20, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirm Password',
+                    hintText: 'Re-enter your Password'),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              child: const Text('Already a member? Login'),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              child: const Text('Sign Up'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignupScreen()),
+                );
+                // Implement signup logic here
+              },
+            ),
+            // Add your form fields and buttons here
+          ],
+        ),
+      ),
+    );
   }
 }
