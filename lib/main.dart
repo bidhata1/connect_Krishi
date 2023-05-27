c18150490be7bdf43059f09606f25b9d03370946
xanyl <aniltiwari645@gmail.com>
2023-05-27 12:20:36 +0545

    Changes in all screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:connect_krishi/screens/splash_screen.dart';
-import 'package:flutter/services.dart';
 import 'package:connect_krishi/screens/login.dart';
+import 'package:connect_krishi/screens/signup.dart';
+import 'package:connect_krishi/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent, // Set status bar color
     statusBarBrightness: Brightness.light, // Set status bar brightness
   ));
-  runApp(const LandingPage());
+  runApp(const MyApp());
 }
 
-class LandingPage extends StatelessWidget {
-  const LandingPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Connect Krishi',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LoginScreenApp(),
+      routes: {
+        '/': (context) => const Center(child: SplashScreen()),
+        '/login':(context) => LoginScreenApp(),
+        '/signup': (context) => const SignupScreen()
+      },
     );
   }
 }
